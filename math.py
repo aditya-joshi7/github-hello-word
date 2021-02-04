82ab035b42891dc7fe0dd0993541c02d2958b58a
aditya-joshi7 <joshiaditya73@yahoo.com>
2021-02-05 01:23:05 +0530

    Commit final

--- math.py
+++ math.py
@@ -1,13 +1,10 @@
 def add(x,y):
     return x+y
 def subtract(x,y):
-<<<<<<< HEAD
-    return x-y
-=======
     if x<y:
     	return ERROR
     return x-y	
->>>>>>> bug123
+
 def multiply(x,y):
     return x*y 
 def divide(x,y):
